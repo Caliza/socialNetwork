ead774b70bf9efea9b47d19975e1709d66d83bdb
Caliza <rances.rodriguez@gmail.com>
2023-04-13 12:49:37 -0700

    Init.

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+      }],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id:false
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController.js');
+
+// /api/courses
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/Thoughts/:ThoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+router.route('/:thoughtId/reaction').post(addReaction);
+router.route('/:thoughtId/reaction/:reactionId').delete(deleteReaction)
+module.exports = router;
